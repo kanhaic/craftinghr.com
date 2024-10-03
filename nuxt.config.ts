6e9307e6973a30645c5db4efda1bdae1f4ff45cb
Kanhai Chhugani <kchhugani@streetlightsoftware.com>
2024-10-04 00:25:50 +0530

    added seo meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'CraftingHR Software Solutions',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'CraftingHR Software Solutions Website' },
+        { name: 'keywords', content: 'CraftingHR, Software Solutions, Website' },
+        { name: 'author', content: 'CraftingHR' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    },
+  
+  }
 })
