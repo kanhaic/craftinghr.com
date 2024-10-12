72a4f37f7ede4684b7bfc0abd3691ba2720f8526
Kanhai Chhugani <kchhugani@streetlightsoftware.com>
2024-10-12 16:49:52 +0530

    add contact as section

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -1,16 +1,156 @@
 <template>
-  <div class="bg-white dark:bg-gray-900 py-20">
-    <section id="contact" class="container mx-auto px-4 text-center">
-      <h2 class="text-3xl font-bold mb-8 dark:text-white">{{ title }}</h2>
-        <p class="text-xl mb-8 dark:text-gray-400">{{ subtitle }}</p>
-        <a
-          :href="'mailto:' + email"
-          class="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 transition duration-300 dark:bg-blue-400 dark:text-white dark:hover:bg-blue-500 dark:hover:text-white  "
+  <section
+    class="relative py-12 overflow-hidden bg-white dark:bg-black sm:py-16 lg:py-20 xl:py-24 border-gray-200 dark:border-gray-800"
+  >
+    <div class="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div class="max-w-sm mx-auto text-center">
+        <h2
+          class="text-4xl font-normal text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
         >
-          {{ ctaText }}
-        </a>
-    </section>
-  </div>
+          Contact us
+        </h2>
+        <p
+          class="mt-4 text-base font-normal text-gray-600 dark:text-gray-400 sm:text-lg sm:mt-6"
+        >
+          We're here to help! Questions about our products, need assistance,
+          don't hesitate to reach out.
+        </p>
+      </div>
+
+      <div
+        class="grid max-w-md grid-cols-1 mx-auto mt-8 overflow-hidden text-center bg-white dark:bg-black border border-gray-200 dark:border-gray-800 bg-opacity-80 md:text-left sm:mt-12 lg:mt-16 md:grid-cols-3 rounded-xl filter backdrop-blur-lg md:max-w-none md:mx-0"
+      >
+        <div class="">
+          <div class="p-8 lg:p-12 xl:p-16">
+            <svg
+              class="w-12 h-12 mx-auto md:mx-0 text-gray-900 dark:text-white"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <p
+              class="mt-8 text-xl font-normal text-gray-900 dark:text-white lg:mt-12 lg:text-2xl"
+            >
+              Email Address
+            </p>
+            <p class="text-base font-normal mt-0.5">
+              <a
+                href="mailto:info@craftinghr.com"
+                title=""
+                class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+              >
+                info (at) craftinghr.com
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="border-t border-gray-200 dark:border-gray-800 md:border-l md:border-t-0"
+        >
+          <div class="p-8 lg:p-12 xl:p-16">
+            <svg
+              class="w-12 h-12 mx-auto md:mx-0 text-gray-900 dark:text-white"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3 5C3 3.89543 3.89543 3 5 3H8.27924C8.70967 3 9.09181 3.27543 9.22792 3.68377L10.7257 8.17721C10.8831 8.64932 10.6694 9.16531 10.2243 9.38787L7.96701 10.5165C9.06925 12.9612 11.0388 14.9308 13.4835 16.033L14.6121 13.7757C14.8347 13.3306 15.3507 13.1169 15.8228 13.2743L20.3162 14.7721C20.7246 14.9082 21 15.2903 21 15.7208V19C21 20.1046 20.1046 21 19 21H18C9.71573 21 3 14.2843 3 6V5Z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <defs>
+                <linearGradient
+                  id="c32"
+                  x1="3"
+                  y1="3"
+                  x2="23.1826"
+                  y2="5.92138"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset="0%" style="stop-color: var(--color-cyan-500)" />
+                  <stop
+                    offset="100%"
+                    style="stop-color: var(--color-purple-500)"
+                  />
+                </linearGradient>
+              </defs>
+            </svg>
+
+            <p
+              class="mt-8 text-xl font-normal text-gray-900 dark:text-white lg:mt-12 lg:text-2xl"
+            >
+              Call Us
+            </p>
+            <p class="text-base font-normal mt-0.5">
+              <a
+                href="tel:+919930205985"
+                title=""
+                class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+              >
+                +91 99302 05985
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="border-t border-gray-200 dark:border-gray-800 md:border-l md:border-t-0"
+        >
+          <div class="p-8 lg:p-12 xl:p-16">
+            <svg
+              class="w-12 h-12 mx-auto md:mx-0"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+
+            <p
+              class="mt-8 text-xl font-normal text-gray-900 dark:text-white lg:mt-12 lg:text-2xl"
+            >
+              India - HQ
+            </p>
+            <p class="mt-5 text-base font-normal">
+              <a
+                href="https://maps.app.goo.gl/RxgwJgq4JWWKWtav7"
+                target="_blank"
+                title=""
+                class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+              >
+                C-319,Kailash Esplanade, LBS Marg, Ghatkopar (West), Mumbai –
+                400086
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
